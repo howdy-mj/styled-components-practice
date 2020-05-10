6dd5db0ed845259f0ce879d0cc099ea047842c7c
howdy-mj <hi.minjungkim@gmail.com>
2020-05-10 22:01:03 +0900

    createGlobalStyle and clone style

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 import React, { Component, Fragment } from 'react';
-import styled from "styled-components";
+import styled, { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    padding: 0;
+    margin: 0;
+  }
+`
 
 function App() {
   return (
     <div className="App">
+      <GlobalStyle />
       <Container>
       <h1>Styled Components</h1>
         <Button success>Hello</Button>
         <Button danger>Hello</Button>
-        {/* <button className="button -success">Hello</button>
-        <button className="button -danger">Hello</button> */}
+        <Anchor as="a" href="https://google.com">Go to Google</Anchor>
       </Container>
     </div>
   );
@@ -23,7 +30,6 @@ background-color: skyblue;
 
 const Button = styled.button`
   border-radius: 50px;
-  border: none;
   padding: 5px;
   min-width: 120px;
   color: white;
@@ -34,6 +40,10 @@ const Button = styled.button`
   }
 
   background-color: ${props => props.danger ? "#e74c3c" : "#2ecc71" }
+`
+
+const Anchor = styled(Button)`
+  text-decoration: none;
 `
 
 export default App;
