33be898babb9231155f2bdd38ca83a21b05bbf2a
howdy-mj <hi.minjungkim@gmail.com>
2020-05-10 21:48:48 +0900

    make button with styled components

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component, Fragment } from 'react';
+import styled from "styled-components";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Container>
+      <h1>Styled Components</h1>
+        <Button success>Hello</Button>
+        <Button danger>Hello</Button>
+        {/* <button className="button -success">Hello</button>
+        <button className="button -danger">Hello</button> */}
+      </Container>
     </div>
   );
 }
+
+const Container = styled.div`
+height: 50vh;
+width: 100%;
+background-color: skyblue;
+`
+
+const Button = styled.button`
+  border-radius: 50px;
+  border: none;
+  padding: 5px;
+  min-width: 120px;
+  color: white;
+  font-weight: 600;
+  cursor: pointer;
+  &:active, &:focus {
+    outline: none;
+  }
+
+  background-color: ${props => props.danger ? "#e74c3c" : "#2ecc71" }
+`
 
 export default App;
